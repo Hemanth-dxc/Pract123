38acae368bc7869b9bde123a7aa3375b25f57adb
devopshemanth <devopshemanth08@gmail.com>
2019-09-12 15:16:57 +0530

    first commit

---
+++ function.py
@@ -0,0 +1,19 @@
+def function() :
+    print("hi to the programer")
+
+print(" top hi")
+function()
+print("bottom hi")
+
+def fun(name , age):
+    print("say hi to " +name + "  " +age +"")
+
+fun("hemu" , "29")
+
+# retun key word in the function
+
+def cube(num):
+    print("this is the cube fucntion ")
+    return num*num*num
+
+print(cube(4))
